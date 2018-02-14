b36876a3de59bd9eae19e96c11c2bb91acb3830b
Douglas Farinelli <douglas.farinelli@gmail.com>
2018-02-13 23:49:15 -0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(
+        os.path.join(
+            os.path.dirname(__file__), fname)
+        ).read()
+
+
+setup(
+    name='awsenviron',
+    license='MIT',
+    version='0.0.0',
+    url='https://github.com/douglasfarinelli/python-awsenviron',
+    author='Douglas Farinelli',
+    author_email='douglas.farinelli@gmail.com',
+    keywords='env python aws parameter-store',
+    description='Load environ vars from AWS Parameter Store',
+    long_description=read('README.md'),
+    py_modules=['awsenviron'],
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
