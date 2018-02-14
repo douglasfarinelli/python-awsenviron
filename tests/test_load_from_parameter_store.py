2774cce2971b92885c753f6fbb6005d2e84cf2b6
Douglas Farinelli <douglas.farinelli@gmail.com>
2018-02-13 23:48:54 -0200

    Add tests

---
+++ tests/test_load_from_parameter_store.py
@@ -0,0 +1,75 @@
+import os
+try:
+    from mock import patch
+except ImportError:
+    from unittest.mock import patch
+
+from boto3.exceptions import Boto3Error
+import pytest
+import awsenviron
+
+
+class SSMStub:
+
+    def __init__(self, response):
+        self.response = response
+
+    def get_parameters_by_path(
+        self,
+        Path=None,
+        WithDecryption=None
+    ):
+
+        if isinstance(self.response, Exception):
+            raise self.response
+
+        return self.response
+
+
+def test_should_raises_runtime_error_on_request():
+    with patch(
+        'awsenviron.boto3.client',
+        return_value=SSMStub(response=Boto3Error())
+    ):
+        with pytest.raises(EnvironmentError):
+            awsenviron.load_from_parameter_store(path='/FOO/')
+
+
+def test_should_raises_runtime_error_if_empty_parameters():
+    with patch(
+        'awsenviron.boto3.client',
+        return_value=SSMStub({
+            'Parameters': [],
+            'NextToken': '69330752-8027-4e7d-9a58-346510f63d28'
+        })
+    ):
+        with pytest.raises(EnvironmentError):
+            awsenviron.load_from_parameter_store(path='/FOO/')
+
+
+def test_should_loads_environ_with_parameters():
+    response = {
+        'Parameters': [
+            {
+                'Name': '/FOO/STAGING/DEBUG',
+                'Type': 'String',
+                'Value': 'yes',
+                'Version': 1
+            },
+            {
+                'Name': '/FOO/STAGING/DATABASE_URI',
+                'Type': 'SecureString',
+                'Value': 'sqlite:///',
+                'Version': 1
+            },
+        ],
+        'NextToken': '69330752-8027-4e7d-9a58-346510f63d28'
+    }
+
+    with patch(
+        'awsenviron.boto3.client',
+        return_value=SSMStub(response)
+    ):
+        awsenviron.load_from_parameter_store(path='/FOO/STAGING/')
+        assert os.environ['DEBUG'] == 'yes'
+        assert os.environ['DATABASE_URI'] == 'sqlite:///'
